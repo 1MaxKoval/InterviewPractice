dcf6979fb372f5a06342f2c27f88e9fa3436f639
Max Koval <31274864+NoiceName@users.noreply.github.com>
2021-01-26 15:13:48 +0100

    To Finish working on the graph

--- trees_and_graphs/BuildOrder.java
+++ trees_and_graphs/BuildOrder.java
@@ -2,7 +2,10 @@ package trees_and_graphs;
 
 import java.util.*;
 
+import jdk.internal.jshell.tool.resources.l10n;
+
 public class BuildOrder {
+    int workingNode;
 
     public void main(String[] args) {
         //Create graph
@@ -10,6 +13,60 @@ public class BuildOrder {
         Scanner scanner = new Scanner(System.in);
 
         String line = scanner.nextLine();
+
+        Graph graph = new Graph();
+        graph.generateGraph(line);
+        ArrayList<Project> noDependencyProjects = nodesWithNoDependencies(graph);
+        StringManager result = new StringManager();
+        updateDependencyList(noDependencyProjects, 0, result);
+    }
+
+    public static int updateDependencyList(ArrayList<Project> nodesWithNoDependency, int workingPointer, StringManager result) {
+        int arraySize = nodesWithNoDependency.size();
+        for (int i = workingPointer; i!=arraySize; i++) {
+            //Integrate string manager.
+            Project workingNode = nodesWithNoDependency.get(i);
+            for (Project childNode : workingNode.children) {
+                childNode.decerementDependencies();
+
+                if (childNode.getDependencies() == 0) {
+                    nodesWithNoDependency.add(childNode);
+                    arraySize++;
+                }
+                
+            }
+            workingPointer++;
+        }
+        return workingPointer;
+    }
+    
+    class StringManager {
+        StringBuilder order = new StringBuilder();
+        
+        public void addProject(Character project) {
+            order.append(project);
+            order.append(',');
+            order.append(' ');
+        }
+
+        public String getString() {
+            return new String(this.order);
+        }
+
+    }
+
+
+    public ArrayList<Project> nodesWithNoDependencies(Graph graph) {
+        HashMap<Character, Project> nodes = graph.getNodes();
+        ArrayList<Project> results = new ArrayList<>();
+
+        for (Project project : nodes.values()) {
+            if (project.getDependencies() == 0) {
+                results.add(project);
+            }
+        }
+
+        return results;
     }
 
 
@@ -18,6 +75,18 @@ public class BuildOrder {
 class Graph {
     HashMap<Character,Project> nodes = new HashMap<>();
 
+    //Ensures unique keys
+    public Project addNode(Character newNode) {
+        Project result = null;
+        if (nodes.containsKey(newNode)) {
+            result = nodes.get(newNode);
+        }
+        else {
+            result = new Project(newNode);
+            nodes.put(newNode,result);
+        }
+        return result;
+    }
 
     public Graph generateGraph(String input) {
         char[] inputArr = input.toCharArray();
@@ -27,31 +96,16 @@ class Graph {
             Character parent = inputArr[i];
             Character child = inputArr[i+2];
 
-            Project parentNode;
-            Project childNode;
-
-            if (nodes.containsKey(parent)) {
-                parentNode = nodes.get(parent);
-            }
-            else {
-                parentNode = new Project(parent);
-                nodes.put(parent, parentNode);
-            }
-
-            if (nodes.containsKey(child)) {
-                childNode = nodes.get(child);
-            }
-            else {
-                childNode = new Project(child);
-                nodes.put(child, childNode);
-            }
+            Project parentNode = addNode(parent);
+            Project childNode = addNode(child);
+            
             childNode.incrementDependencies();
             parentNode.addChild(childNode);
         }
         return  this;
     }
 
-
+    public HashMap<Character, Project> getNodes() {return this.nodes;}
 
 }
 
